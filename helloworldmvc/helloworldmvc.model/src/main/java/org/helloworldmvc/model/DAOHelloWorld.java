a63dda165e0edb516dca990d09b8456706c86f50
ClementFbt <clement.froz@gmail.com>
2017-06-06 15:31:21 +0200

    phase3

---
+++ helloworldmvc/helloworldmvc.model/src/main/java/org/helloworldmvc/model/DAOHelloWorld.java
@@ -0,0 +1,31 @@
+package org.helloworldmvc.model;
+
+public class DAOHelloWorld {
+	private final String		helloWorldMessage	= null;
+	private final DAOHelloWorld	instance			= null;
+	private final String		FileName			= "HelloWorld.txt";
+
+	public DAOHelloWorld() {
+
+	}
+
+	public String getHelloWorldMessage() {
+		return this.helloWorldMessage;
+	}
+
+	public void setHelloWorldMessage(String helloWorldMessage) {
+
+	}
+
+	public DAOHelloWorld getInstance() {
+		return this.instance;
+	}
+
+	public void setInstance(DAOHelloWorld instance) {
+
+	}
+
+	public void readFile() {
+
+	}
+}
