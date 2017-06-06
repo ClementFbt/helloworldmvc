949eac1a5e129e49bf1c8a7b9297083356d859f3
ClementFbt <clement.froz@gmail.com>
2017-06-06 23:46:22 +0200

    phse4/5

--- helloworldmvc/helloworldmvc.view/src/main/java/org/helloworldmvc/view/View.java
+++ helloworldmvc/helloworldmvc.view/src/main/java/org/helloworldmvc/view/View.java
@@ -3,7 +3,7 @@ package org.helloworldmvc.view;
 import org.helloworldmvc.Contract.IView;
 
 public class View implements IView {
-	public void displayMessage(String message) {
+	public void displayMessage(final String message) {
 		System.out.println(message);
 	}
 }
